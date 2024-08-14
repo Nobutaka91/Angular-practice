339c5560a522d2a3f9c8085fbd873cf85d9ec1af
NobutakaSuzuki <suzuki_n@opening-line.co.jp>
2024-08-14 23:31:43 +0900

    チャットアプリ画面(タイムライン)の表示

--- angular-chat/src/app/app.component.ts
+++ angular-chat/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+const  COMMENTS: Comment[] = [
+  { name: 'Kou Ackerman', message: 'Hi'},
+  { name: 'Kou Ackerman', message: 'Long time no see!'},
+  { name: 'Kou Ackerman', message: 'Did u finish reading all these AOT manga?'},
+]
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
 }
