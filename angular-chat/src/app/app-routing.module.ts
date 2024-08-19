83b4714f5dd0bff6075cf4f7d262ee9dccec4ddf
NobutakaSuzuki <suzuki_n@opening-line.co.jp>
2024-08-19 21:53:42 +0900

    Firebaase Authenticationの設定

--- angular-chat/src/app/app-routing.module.ts
+++ angular-chat/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 import { NotFuundComponent } from './core/components/not-fuund/not-fuund.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: ChatComponent },
+  { path: 'signup', component: SignUpComponent },
   { path: '**', component: NotFuundComponent },
 ]
 
